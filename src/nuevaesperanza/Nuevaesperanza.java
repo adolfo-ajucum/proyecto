fbb48eccf1a13cd1bb6f0b27697d47431b6606e0
Martin <martin_aj22@hotmail.com>
2019-05-24 10:39:12 -0600

    Version 1 de bd e implementación de dashboard

--- src/nuevaesperanza/Nuevaesperanza.java
+++ src/nuevaesperanza/Nuevaesperanza.java
@@ -5,13 +5,17 @@
  */
 package nuevaesperanza;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -20,25 +24,15 @@ import javafx.stage.Stage;
 public class Nuevaesperanza extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    public void start(Stage stage) throws IOException {
+       Parent root = FXMLLoader.load(getClass().getResource("/Dashboard/Dashboard.fxml"));
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        stage.initStyle(StageStyle.TRANSPARENT);
+        Scene scene = new Scene(root);
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.setResizable(true);
+        stage.show();
     }
 
     /**
